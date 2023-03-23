1115b5fb9cd3e912b6a1c70efdbd3fdefc9eb7eb
马军 <majun@xiwicloud.com>
2023-03-23 18:12:40 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
+import btnCom from "btn-com";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 Vue.use(ElementUI);
+
+Vue.use(btnCom);
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
